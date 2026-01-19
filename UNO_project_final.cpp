ad16f7269c48d5b269fcac80b07f2f0af535e0c6
rangelparishev <rangelparishev@gmail.com>
2026-01-19 12:17:44 +0200

    Commit 6
    Added commentary block

--- UNO_project_final.cpp
+++ UNO_project_final.cpp
@@ -1,3 +1,17 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2025/2026
+*
+* @author Rangel Parishev
+* @idnumber 0MI0600668
+* @compiler VS
+*
+* <c++ file with project>
+*
+*/
 // ---------- Libraries ----------
 #include <iostream>
 #include <fstream>
